97b3d1756d6589bbc861a8b76f98355f12ddac49
Evy Bongers <e.bongers@pararius.nl>
2021-07-06 12:10:21 +0200

    Add optional attribute retry_policy

--- pubsub.tf
+++ pubsub.tf
@@ -15,4 +15,16 @@ resource "google_pubsub_subscription" "subscription" {
   message_retention_duration = each.value.message_retention_duration == null ? null : "${each.value.message_retention_duration}s"
   ack_deadline_seconds       = each.value.acknowledge_deadline
   labels                     = each.value.labels
+
+  dynamic "retry_policy" {
+    for_each = each.value.dead_letter_policy == null ? [] : [1]
+    iterator = policy
+
+    content {
+      minimum_backoff = try(policy.minimum_backoff, null)
+      maximum_backoff = try(policy.maximum_backoff, null)
+    }
+  }
 }
+
+data "google_project" "current" {}
