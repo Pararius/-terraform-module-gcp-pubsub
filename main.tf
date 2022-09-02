7d921119773624bea2029afcfd0da01064d5707f
Danilo Bettencourt <d.bettencourt@treehouse.nl>
2022-09-02 10:51:16 +0200

    allow users to use a different provider other than default

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ terraform {
     google = {
       source  = "hashicorp/google"
       version = ">= 3.56.0"
+      configuration_aliases = [ google.alternate ]
     }
   }
   experiments = [module_variable_optional_attrs]
