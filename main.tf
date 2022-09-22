fa580e3ddfa311becadb80cfaddc8f41cc52cf43
Ruben Laban <r.laban@treehouse.nl>
2022-09-22 11:13:44 +0200

    Optional attributes are no longer experimental as of 1.3.0

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.3.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = ">= 3.56.0"
     }
   }
-  experiments = [module_variable_optional_attrs]
 }
