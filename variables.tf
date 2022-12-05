0426e2ff5b6bc313e149b45f9c9bfc2f0383e72b
Ruben Laban <r.laban@treehouse.nl>
2022-12-05 16:08:21 +0100

    Add support for retain_acked_messages

--- variables.tf
+++ variables.tf
@@ -9,18 +9,19 @@ variable "labels" {
 
 variable "subscriptions" {
   type = list(object({
-    subscription_name          = string
-    acknowledge_deadline       = number
-    message_retention_duration = optional(number)
-    labels                     = optional(map(string))
+    acknowledge_deadline = number
     dead_letter_policy = optional(object({
       topic_name            = optional(string)
       max_delivery_attempts = optional(number)
     }))
+    labels                     = optional(map(string))
+    message_retention_duration = optional(number)
+    retain_acked_messages      = bool
     retry_policy = optional(object({
       minimum_backoff = optional(number)
       maximum_backoff = optional(number)
     }))
+    subscription_name = string
   }))
 }
 
