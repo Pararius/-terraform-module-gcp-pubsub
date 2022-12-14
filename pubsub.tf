fdbce42c35ae5bc23bd03f956ed99da088525f24
Ruben Laban <5331085+tun0@users.noreply.github.com>
2022-12-14 13:15:38 +0100

    Add support for retain_acked_messages (#5)
    * Add support for retain_acked_messages
    
    * optional

--- pubsub.tf
+++ pubsub.tf
@@ -12,9 +12,10 @@ resource "google_pubsub_subscription" "subscription" {
   name    = each.key
   topic   = google_pubsub_topic.topic.name
 
-  message_retention_duration = each.value.message_retention_duration == null ? null : "${each.value.message_retention_duration}s"
   ack_deadline_seconds       = each.value.acknowledge_deadline
   labels                     = each.value.labels
+  message_retention_duration = each.value.message_retention_duration == null ? null : "${each.value.message_retention_duration}s"
+  retain_acked_messages      = each.value.retain_acked_messages
 
   dynamic "dead_letter_policy" {
     for_each = each.value.dead_letter_policy == null ? [] : [1]
